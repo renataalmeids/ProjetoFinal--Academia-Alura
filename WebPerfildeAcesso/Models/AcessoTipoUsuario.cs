110b54fc350381a0fcb0ae44cedca01a494b10d4
ProjetoCSarpp-Alura <ras_almeida@hotmail.com>
2020-10-28 16:49:35 -0300

    Criando classes perfil, acesso e tipo de usuário

---
+++ WebPerfildeAcesso/Models/AcessoTipoUsuario.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace WebPerfildeAcesso.Models
+{
+    public class AcessoUsuario
+    {
+        [Display(Name = "Credencial")]
+        [Column("Id")]
+        public int Id { get; set; }
+
+        [Display(Name = "Cargo")]
+        [Column("NomeOcupação")]
+        public string NomeOcupação { get; set; }
+
+        [Display(Name = "TipoUsuario")]
+        [ForeignKey("TipoUsuario")]
+        [Column(Order = 1)]
+        public int IdTipoUsuario { get; set; }
+
+        public virtual TipoUsuario TipoUsuario { get; set; }
+    }
+
+}
---
+++ WebPerfildeAcesso/Models/TipoUsuario.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace WebPerfildeAcesso.Models
+{
+    [Table("TipoUsuario")]
+    public class TipoUsuario
+    {
+        [Display(Name = "Credencial")]
+
+        [Column("Id")]
+        public int Id { get; set; }
+
+
+        [Display(Name = "Usuario")]
+        [Column("TipoUsuario")]
+        public string Tipousuario { get; set; }
+    }
+
+}

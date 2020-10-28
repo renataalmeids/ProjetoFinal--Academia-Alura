68ee55450d4291c26a054aa85d6c4c885fac7f36
ProjetoCSarpp-Alura <ras_almeida@hotmail.com>
2020-10-28 12:54:19 -0300

    Primeiro Commit

---
+++ WebPerfildeAcesso/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(WebPerfildeAcesso.Startup))]
+namespace WebPerfildeAcesso
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
